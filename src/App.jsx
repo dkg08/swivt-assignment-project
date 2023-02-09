c0b3afed39955cec933d415ea1f23c1d89058f59
dilipgiri <dilipgirikun.23@gmail.com>
2023-02-09 08:56:06 +0545

    fix: changes to src and public files

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-
+import './App.scss'
+import { Topbar } from './components/topbar/Topbar';
+import { Navbar } from './components/navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-      
+      <Topbar />
+      <Navbar />
     </div>
   );
 }
