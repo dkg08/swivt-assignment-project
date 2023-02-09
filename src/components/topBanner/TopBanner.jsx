9fd0fe48f488201e2755ca780f75592515282409
dilipgiri <dilipgirikun.23@gmail.com>
2023-02-09 09:41:21 +0545

    comp: topBanner component created

---
+++ src/components/topBanner/TopBanner.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './topBanner.scss'
+
+export const TopBanner = () => {
+  return (
+    <div className='topBanner'>
+        <div className="bannerImageContainer">
+
+        </div>
+    </div>
+  )
+}
